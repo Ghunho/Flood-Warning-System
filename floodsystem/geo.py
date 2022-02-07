84c48a15ae26634859fc14f456be7fce4a350d6d
Ghunho Min <gm701@cam.ac.uk>
2022-02-07 23:19:59 +0000

    Tasks 1D and 1E

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -17,12 +17,25 @@ def stations_by_distance(stations, p):
 def stations_within_radius(stations, centre, r):
     station_distances = stations_by_distance(stations, centre)
     return [entry[0] for entry in station_distances if entry[1] <= r]
-
+    
 def rivers_with_station(stations):
-    return sorted(list(set([station.river for station in stations])))
+    rivers=[]
+    for station in stations:
+        if not station.river in rivers:
+            rivers.append(station.river)
+    return rivers
 
 def stations_by_river(stations):
-    rivers_stations_dict = {}
+    stations_in_river={}
     for river in rivers_with_station(stations):
-        rivers_stations_dict[river] = sorted([station.name for station in stations if station.river == river])
-    return rivers_stations_dict
+        stations_list = []
+        for station in stations_list:
+            if station.river == river:
+                stations_list.append(station)
+        stations_in_river[river] = stations_list
+    return stations_in_river
+
+def rivers_by_station_number(stations, N):
+    stations_list = list()
+
+
