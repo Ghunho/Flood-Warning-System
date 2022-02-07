5e5b1cc771ef2c8754ebd51d7bf54c9251dba4b2
Shan <ssp38@cam.ac.uk>
2022-02-07 22:17:11 +0000

    Complete task1D

---
+++ task1D.py
@@ -0,0 +1,20 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+
+def run():
+    stations = build_station_list()
+
+    rivers = rivers_with_station(stations)
+    print("\n {} stations. First 10 - {}".format(len(rivers), rivers[:10]))
+
+    rivers_stations_dict = stations_by_river(stations)
+    print("\n Stations next to River Aire: {}".format(rivers_stations_dict['River Aire']))
+    print("\n Stations next to River Cam: {}".format(rivers_stations_dict['River Cam']))
+    print("\n Stations next to River Thames: {}".format(rivers_stations_dict['River Thames']))
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
---
+++ test_geo.py
@@ -0,0 +1,9 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
+from floodsystem.station import MonitoringStation
+
+def test_stations_by_distance(): 
+    x = stations_by_distance(stations, p)
+
+def test_stations_within_radius():
+    x = stations_within_radius(stations, centre, r)
