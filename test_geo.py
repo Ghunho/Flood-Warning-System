7fb986e59bba5474638a04565a2caab656b8b0d6
Shan <ssp38@cam.ac.uk>
2022-02-08 12:48:31 +0000

    delete testgeo

--- test_geo.py
+++ test_geo.py
@@ -1,9 +1,3 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.geo import stations_within_radius
 from floodsystem.station import MonitoringStation
-
-def test_stations_by_distance(): 
-    
-
-def test_stations_within_radius():
-   
